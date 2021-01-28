d7fae62d9870a0c4be3faf72782e570f335788e6
Nima <nima@movio.co>
2021-01-28 18:42:04 +1300

    add logger and update interval

--- src/WorkboxProvider.tsx
+++ src/WorkboxProvider.tsx
@@ -2,36 +2,46 @@ import React, {FC, useEffect, useState} from 'react';
 import {Workbox} from 'workbox-window';
 
 import WorkboxContext from './WorkboxContext';
+import logger from './logger';
+import {WorkboxLifecycleWaitingEvent} from 'workbox-window/utils/WorkboxEvent';
 
 const WorkboxProvider: FC<{serviceWorkerUrl?: string; interval?: number}> = ({
     children,
     serviceWorkerUrl = `/service-worker.js`,
-    // interval = 60 * 60 * 1000, // 1 hour
+    interval = 60 * 60 * 1000, // 1 hour
 }) => {
-    // let timer: number | null = null;
     const [isUpdateWaiting, setUpdateWaiting] = useState(false);
-    const [workbox, setWorkbox] = useState<Workbox | undefined>(undefined);
-    const [registration, setRegistration] = useState<ServiceWorkerRegistration | undefined>(undefined);
+    const [workbox, setWorkbox] = useState<Workbox | undefined>();
+    const [registration, setRegistration] = useState<ServiceWorkerRegistration | undefined>();
 
     useEffect(() => {
-        console.log('USE-EFFECT');
-        if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
-            const wb = new Workbox(serviceWorkerUrl);
-            setWorkbox(wb);
-            // @ts-ignore
-            const showSkipWaitingPrompt = (event) => {
-                console.log('showSkipWaitingPrompt', event.wasWaitingBeforeRegister, event);
-                setUpdateWaiting(true);
-                // `event.wasWaitingBeforeRegister` will be false if this is
-            };
-
-            // Add an event listener to detect when the registered
-            // service worker has installed but is waiting to activate.
-            // @ts-ignore
-            wb.addEventListener('waiting', (e) => console.log('WAITING', e) || showSkipWaitingPrompt(e));
-
-            wb.register().then((r) => setRegistration(r));
+        logger.log('USE-EFFECT');
+        if (process.env.NODE_ENV !== 'production' || !('serviceWorker' in navigator)) {
+            return;
         }
+
+        const timer = Boolean(interval) && setInterval(() => wb.update(), interval);
+
+        const wb = new Workbox(serviceWorkerUrl);
+        setWorkbox(wb);
+        const showSkipWaitingPrompt = (event: WorkboxLifecycleWaitingEvent) => {
+            logger.log('showSkipWaitingPrompt', event.wasWaitingBeforeRegister, event);
+            setUpdateWaiting(true);
+            // `event.wasWaitingBeforeRegister` will be false if this is
+        };
+
+        // Add an event listener to detect when the registered
+        // service worker has installed but is waiting to activate.
+        wb.addEventListener('waiting', (e) => {
+            logger.log('WAITING', e);
+            showSkipWaitingPrompt(e);
+        });
+
+        wb.register().then((r) => setRegistration(r));
+
+        return () => {
+            if (timer) clearInterval(timer);
+        };
     }, []);
 
     return (
@@ -43,11 +53,11 @@ const WorkboxProvider: FC<{serviceWorkerUrl?: string; interval?: number}> = ({
                         return;
                     }
 
-                    console.log('ACTIVATE UPDATE', registration, workbox);
+                    logger.log('ACTIVATE UPDATE', registration, workbox);
                     workbox.addEventListener('controlling', (e) => {
                         // would be called when wasWaitingBeforeRegister = true
                         // would be called when isExternal = false
-                        console.log('CONTROLLING', e, e.isExternal);
+                        logger.log('CONTROLLING', e, e.isExternal);
                         if (!e.isExternal) {
                             // <---- may need to be removed
                             // To avoid calling reload twice, needs to check isExternal
@@ -57,7 +67,7 @@ const WorkboxProvider: FC<{serviceWorkerUrl?: string; interval?: number}> = ({
                     workbox.addEventListener('activated', (e) => {
                         // would be called when isExternal = true
                         // would be called when isExternal = false
-                        console.log('ACTIVATED', e, e.isExternal);
+                        logger.log('ACTIVATED', e, e.isExternal);
                         if (e.isExternal) {
                             // REF: https://developers.google.com/web/tools/workbox/modules/workbox-window#when_an_unexpected_version_of_the_service_worker_is_found
                             // When it is external worker, it would not trigger "controlling" event
@@ -67,7 +77,7 @@ const WorkboxProvider: FC<{serviceWorkerUrl?: string; interval?: number}> = ({
                     });
 
                     if (registration.waiting) {
-                        console.log('workbox MESSAGESKIPWAITING');
+                        logger.log('workbox MESSAGESKIPWAITING');
                         workbox.messageSkipWaiting();
                         setUpdateWaiting(false);
                     } else {
